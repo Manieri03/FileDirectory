ade68fde0cfb0aa54ab939761e49b2e129543f8b
Alessio Manieri <mani.alexander03@gmail.com>
2020-11-29 15:16:57 +0100

    Aggiungere i file di progetto.

---
+++ FileAndDirectoryLibrary/File.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAndDirectoryLibrary
+{
+    public class File : FileAstratto
+    {
+        public long dimensione { get; set; }
+
+        public File(string nome, Directory d) : base(nome, d) { }
+    }
+}
